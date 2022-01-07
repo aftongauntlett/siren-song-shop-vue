644a68fb254112e40eef596b3c80fb9e9a08c532
Afton Gauntlett <afton.gauntlett@gmail.com>
2022-01-07 18:03:26 -0500

    prepping for bootstrap

---
+++ src/components/Footer.vue
@@ -0,0 +1,114 @@
+<template>
+  <b-container class="bv-example-row border-top">
+    <div class="d-flex flex-wrap justify-content-around">
+      <div>
+        <div>
+          <b-img
+            :src="require('@/assets/images/logo.png')"
+            width="200"
+            fluid
+            alt="Potomac Dining Logo"
+            class="image"
+          >
+          </b-img>
+        </div>
+      </div>
+      <div>
+        <h2 class="footer-header pb-3">Company</h2>
+        <div class="links" v-for="link in links" :key="link.name">
+          <b-link :to="{ name: link.name }"> {{ link.display }}</b-link>
+        </div>
+      </div>
+      <div>
+        <div class="contact">
+          <h2 class="footer-header pb-3">Contact</h2>
+          <div class="pb-3">13873 Park Center Road Herndon, Virginia 20171</div>
+          <div>Tel: 240-752-7565</div>
+          <div class="pb-3">Fax: 703-787-6550</div>
+          <b-link class="email-link">info@potomacdining.com</b-link>
+        </div>
+      </div>
+    </div>
+    <div></div>
+    <div class="copyright">
+      © Copyright 2022 - Potomac Family Dining Group - All Rights Reserved
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: "About",
+          display: "About",
+        },
+        {
+          name: "Contact",
+          display: "Contact",
+        },
+        {
+          name: "Locations",
+          display: "Locations",
+        },
+        {
+          name: "Careers",
+          display: "Careers",
+        },
+        {
+          name: "Employee",
+          display: "Employee Login",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.border-top {
+  border-top: 1px solid #ffa500 !important;
+  background-color: #ffffff;
+  padding-bottom: 20px;
+  padding-top: 20px;
+}
+
+.image {
+  padding-top: 30px;
+}
+
+.copyright {
+  border-top: 1px solid #ffa500;
+  margin-top: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: center;
+  font-size: 14px;
+}
+
+.footer-header {
+  font-size: 20px;
+  color: #000080;
+  letter-spacing: 2.5px;
+  text-transform: uppercase;
+  text-align: left;
+  padding-top: 30px;
+  font-weight: 600;
+}
+
+.contact {
+  text-align: left;
+  padding-bottom: 50px;
+}
+
+.links {
+  text-align: left;
+  padding-bottom: 15px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  font-size: 14px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,77 @@
+<template>
+  <b-navbar class="potomac-nav text" sticky>
+    <b-navbar-brand :to="{ name: 'Home' }">
+      <b-img
+        :src="require('@/assets/images/blue-flower.jpeg')"
+        width="200"
+        fluid
+        alt="Responsive image"
+        class="pb-3"
+      ></b-img>
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="potomac-nav"></b-navbar-toggle>
+    <b-collapse id="potomac-nav" is-nav>
+      <b-navbar-nav class="nav-border ml-auto">
+        <b-nav-item-dropdown
+          :to="{ name: 'About' }"
+          class="pr-4"
+          text="About"
+          right
+        >
+          <b-dropdown-item :to="{ name: 'Values' }">
+            Our Values
+          </b-dropdown-item>
+          <b-dropdown-item :to="{ name: 'President' }">
+            Message From President
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item class="pr-4 test" :to="{ name: 'Careers' }"
+          >Careers</b-nav-item
+        >
+        <b-nav-item class="pr-4" :to="{ name: 'Locations' }"
+          >Locations</b-nav-item
+        >
+        <b-nav-item class="pr-4" :to="{ name: 'Contact' }">Contact</b-nav-item>
+        <b-nav-item class="pr-4" :to="{ name: 'Employee' }"
+          >Employee</b-nav-item
+        >
+        <b-nav-item class="pr-4" :to="{ name: 'Login' }">Login</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.potomac-nav {
+  font-family: "Montserrat", sans-serif;
+  letter-spacing: 2.5px;
+  text-transform: uppercase;
+  font-size: 18px;
+  background-color: #ffffff;
+}
+
+.b-nav-item:hover {
+  color: green !important;
+}
+
+.nav-border {
+  border-top: 2px solid green;
+  padding-top: 15px;
+}
+
+.navbar-light .navbar-nav .nav-link {
+  color: #000080;
+}
+
+.navbar-light .navbar-nav .nav-link:hover {
+  color: green;
+  border-bottom: 1px solid green;
+}
+</style>
